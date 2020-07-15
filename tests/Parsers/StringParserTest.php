9682cb058c468329a3273607d2fcb172fdfd9835
Valentin Popov <info@valentineus.link>
2020-07-15 16:54:44 +0400

    Added string parser
    Signed-off-by: Valentin Popov <info@valentineus.link>

---
+++ tests/Parsers/StringParserTest.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Copyright 2020 “EPGService”
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an “AS IS” BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types = 1);
+
+namespace Tests\Parsers;
+
+use EPGService\Parsers\StringParser;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @copyright Copyright © 2020 “Valentin Popov” <info@valentineus.link>
+ * @license   http://www.apache.org/licenses/LICENSE-2.0
+ * @package   Tests\Parsers
+ */
+final class StringParserTest extends TestCase {
+	public function testValid(): void {
+		$string = '<h1>Hello WorldÆØÅ!</h1>';
+		self::assertEquals('Hello World!', StringParser::get($string));
+	}
+}
