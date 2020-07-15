6c52322a3cc5fe4036cc7689d60bd47eca402452
Valentin Popov <info@valentineus.link>
2020-07-15 19:20:43 +0400

    Added genre entity
    Signed-off-by: Valentin Popov <info@valentineus.link>

---
+++ tests/Entities/GenreEntityTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Copyright 2020 “EPGService”
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an “AS IS” BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types = 1);
+
+namespace Tests\Entities;
+
+use EPGService\Entities\GenreEntity;
+use Faker\Factory as Faker;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @copyright Copyright © 2020 “Valentin Popov” <info@valentineus.link>
+ * @license   http://www.apache.org/licenses/LICENSE-2.0
+ * @package   Tests\Entities
+ */
+final class GenreEntityTest extends TestCase {
+	public function testCreateEntity(): void {
+		$faker = Faker::create();
+
+		$id = $faker->numberBetween(1, 999);
+		$lang = $faker->languageCode;
+		$name = $faker->word;
+		$version = $faker->sha256;
+
+		$entity = GenreEntity::create(compact('id', 'lang', 'name', 'version'));
+
+		self::assertEquals($id, $entity->id);
+		self::assertEquals($lang, $entity->lang);
+		self::assertEquals($name, $entity->name);
+		self::assertEquals($version, $entity->version);
+	}
+}
