76e8ed954e2a1c70437fb34a927d1bb1d921d3ce
Valentin Popov <info@valentineus.link>
2020-07-15 13:54:01 +0400

    Added category entity
    Signed-off-by: Valentin Popov <info@valentineus.link>

---
+++ src/Entities/CategoryEntity.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * Copyright 2020 “EPGService”
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an “AS IS” BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types = 1);
+
+namespace EPGService\Entities;
+
+use RuntimeException;
+use function is_int;
+use function is_string;
+
+/**
+ * @property-read int    $id
+ * @property-read string $lang
+ * @property-read string $name
+ * @property-read string $version
+ *
+ * @copyright Copyright © 2020 “Valentin Popov” <info@valentineus.link>
+ * @license   http://www.apache.org/licenses/LICENSE-2.0
+ * @package   EPGService\Entities
+ */
+class CategoryEntity {
+	/**
+	 * @var int
+	 */
+	protected int $id;
+
+	/**
+	 * @var string
+	 */
+	protected string $lang;
+
+	/**
+	 * @var string
+	 */
+	protected string $name;
+
+	/**
+	 * @var string
+	 */
+	protected string $version;
+
+	/**
+	 * @param int    $id
+	 * @param string $lang
+	 * @param string $name
+	 * @param string $version
+	 */
+	protected function __construct(int $id, string $lang, string $name, string $version) {
+		$this->id = $id;
+		$this->lang = $lang;
+		$this->name = $name;
+		$this->version = $version;
+	}
+
+	/**
+	 * @param array $payload
+	 *
+	 * @return \EPGService\Entities\CategoryEntity
+	 * @throws \RuntimeException
+	 */
+	public static function create(array $payload): CategoryEntity {
+		if (!is_int($payload['id'])) {
+			throw new RuntimeException('blah-blah-blah');
+		}
+
+		if (!is_string($payload['lang'])) {
+			throw new RuntimeException('blah-blah-blah');
+		}
+
+		if (!is_string($payload['name'])) {
+			throw new RuntimeException('blah-blah-blah');
+		}
+
+		if (!is_string($payload['version'])) {
+			throw new RuntimeException('blah-blah-blah');
+		}
+
+		return new CategoryEntity($payload['id'], $payload['lang'], $payload['name'], $payload['version']);
+	}
+
+	/**
+	 * @param string $name
+	 *
+	 * @return mixed
+	 */
+	public function __get(string $name) {
+		return $this->$name;
+	}
+
+	/**
+	 * @param string $name
+	 * @param mixed  $value
+	 *
+	 * @throws \RuntimeException
+	 */
+	public function __set(string $name, $value) {
+		throw new RuntimeException('blah-blah-blah');
+	}
+}
