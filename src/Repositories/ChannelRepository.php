82584345f6340d83008bed2eb35b3600da925135
Valentin Popov <info@valentineus.link>
2020-07-17 13:57:33 +0400

    Added channel repository
    Signed-off-by: Valentin Popov <info@valentineus.link>

---
+++ src/Repositories/ChannelRepository.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Copyright 2020 “EPGService”
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an “AS IS” BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types = 1);
+
+namespace EPGService\Repositories;
+
+use EPGService\Entities\ChannelEntity;
+use EPGService\Environments\ServiceEnvironment;
+use EPGService\Parsers\StringParser;
+use GuzzleHttp\Client;
+use RuntimeException;
+use SimpleXMLElement;
+use function is_bool;
+
+/**
+ * @copyright Copyright © 2020 “Valentin Popov” <info@valentineus.link>
+ * @license   http://www.apache.org/licenses/LICENSE-2.0
+ * @package   EPGService\Repositories
+ */
+final class ChannelRepository implements BaseRepository {
+	/**
+	 * @var string
+	 */
+	private const METHOD = 'index';
+
+	/**
+	 * @var \GuzzleHttp\Client
+	 */
+	private Client $client;
+
+	/**
+	 * @param \EPGService\Environments\ServiceEnvironment $environment
+	 */
+	private function __construct(ServiceEnvironment $environment) {
+		$this->client = new Client([
+			'base_uri' => $environment->getUrl(),
+		]);
+	}
+
+	/**
+	 * @param \EPGService\Environments\ServiceEnvironment $environment
+	 *
+	 * @return \EPGService\Repositories\ChannelRepository
+	 */
+	public static function create(ServiceEnvironment $environment): ChannelRepository {
+		return new ChannelRepository($environment);
+	}
+
+	/**
+	 * @return array
+	 *
+	 * @throws \GuzzleHttp\Exception\GuzzleException
+	 * @throws \RuntimeException
+	 */
+	public function get(): array {
+		$response = $this->client->get(self::METHOD);
+		$content = $response->getBody()->getContents();
+		$xml = simplexml_load_string($content);
+
+		if (is_bool($xml) || !$xml->channel instanceof SimpleXMLElement) {
+			throw new RuntimeException('blah-blah-blah');
+		}
+
+		$result = [];
+
+		foreach ($xml->channel as $element) {
+			if (!$element instanceof SimpleXMLElement) {
+				trigger_error('blah-blah-blah', E_USER_WARNING);
+				continue;
+			}
+
+			$result[] = ChannelEntity::create([
+				'base_id'   => StringParser::get($element->{'base-channel'}),
+				'base_name' => StringParser::get($element->{'base-channel'}['id']),
+				'epg_id'    => StringParser::get($element['epgsrvc_id']),
+				'geo_data'  => StringParser::get($element->{'geo-data'}),
+				'href'      => StringParser::get($element->href),
+				'icon'      => StringParser::get($element->icon['src']),
+				'id'        => StringParser::get($element['id']),
+				'lang'      => StringParser::get($element->{'display-name'}['lang']),
+				'name'      => StringParser::get($element->{'base-channel'}),
+				'update_at' => StringParser::get($element->update),
+				'week'      => StringParser::get($element->week),
+			]);
+		}
+
+		return $result;
+	}
+}
---
+++ tests/Repositories/ChannelRepositoryTest.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Copyright 2020 “EPGService”
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an “AS IS” BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types = 1);
+
+namespace Tests\Repositories;
+
+use EPGService\Repositories\ChannelRepository;
+use PHPUnit\Framework\TestCase;
+use Tests\Utilities\GetServiceEnvironment;
+
+/**
+ * @copyright Copyright © 2020 “Valentin Popov” <info@valentineus.link>
+ * @license   http://www.apache.org/licenses/LICENSE-2.0
+ * @package   Tests\Repositories
+ */
+final class ChannelRepositoryTest extends TestCase {
+	public function testGetAction(): void {
+		$env = GetServiceEnvironment::get();
+
+		foreach (ChannelRepository::create($env)->get() as $channel) {
+			/** @var \EPGService\Entities\ChannelEntity $channel */
+
+			self::assertIsString($channel->base_id);
+			self::assertIsString($channel->base_name);
+			self::assertIsString($channel->epg_id);
+			self::assertIsString($channel->geo_data);
+			self::assertIsString($channel->href);
+			self::assertIsString($channel->icon);
+			self::assertIsString($channel->id);
+			self::assertIsString($channel->lang);
+			self::assertIsString($channel->name);
+			self::assertIsString($channel->update_at);
+			self::assertIsString($channel->week);
+		}
+	}
+}
